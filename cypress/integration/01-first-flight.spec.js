e56040b7a6567e651bc23c6f44a34fc8f96fe69b
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-07-16 23:56:03 +1000

    wrote my first cypres test

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,11 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+    beforeEach(() => {
+        cy.visit('/jetsetter');
+    })
+
+    it('should contain Applications as header', () => {
+        cy.contains('h3', 'Applications').should('contain.text', 'Applications');
+    });
+});
