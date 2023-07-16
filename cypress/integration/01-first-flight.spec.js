4a3ccd8e18ea1929f0ce96afe61187dce252cab5
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-07-17 00:08:12 +1000

    wrote test for CSS selector in cypress

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -8,4 +8,9 @@ describe('Create a New Item', () => {
     it('should contain Applications as header', () => {
         cy.contains('h3', 'Applications').should('contain.text', 'Applications');
     });
+
+    it('should highlight the Jetsetter App on the sidenav', () => {
+        cy.get('nav#table-of-contents > ul > li:nth-child(1) > a').should('contain.text', 'Jetsetter');
+        cy.get('nav#table-of-contents > ul > li:nth-child(1)').should('have.class', 'bg-purple-600')
+    })
 });
