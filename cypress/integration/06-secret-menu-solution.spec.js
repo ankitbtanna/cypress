6c05f36d44f2173c249467c525c17f216406abe2
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-08-16 18:47:56 +1000

    programmatically generate tests

--- cypress/integration/06-secret-menu-solution.spec.js
+++ cypress/integration/06-secret-menu-solution.spec.js
@@ -50,4 +50,30 @@ describe('Secret Menu Items', () => {
       cy.get(`#${property}-column`).should('be.hidden');
     })
   }
+
+  for (let restaurant of restaurants) {
+    it(`should only display items only from the ${restaurant} restaurant that is selected`, () => {
+      cy.get('#restaurant-visibility-filter').select(restaurant);
+      cy.get('td[headers="whereToOrder-column"').each(($td) => {
+        cy.wrap($td).find('.cell').should('contain.text', restaurant);
+      });
+    });
+  }
+
+  describe('Rating Filter', () => {
+    beforeEach(() => {
+      cy.get('#minimum-rating-visibility').as('ratingFilter');
+    });
+
+    for (let rating of ratings) {
+      it.only(`should only display items with a rating of ${rating} or higher`, () => {
+        cy.get('@ratingFilter').invoke('val', rating).trigger('change');
+        cy.get('td[headers="popularity-column"').each(($td) => {
+          const ratingText = $td.find('.cell').text();
+          const ratingNumber = parseInt(ratingText);
+          expect(ratingNumber).to.be.gte(rating);
+        });
+      });
+    }
+  })
 });
